182a9de91d3655a5f429f3b2feb174bcb9f49ffe
Ajay Prasath M <ajay3015@global.chainsys.com>
2022-02-23 23:11:22 +0530

    Advice quoteapp

---
+++ Advice/script.js
@@ -0,0 +1,16 @@
+const advice = async()=>{
+    const data = await fetch("https://api.adviceslip.com/advice");
+    const result = await data.json();
+    console.log(result);
+    var id = result.slip.id;
+    var quotes = result.slip.advice
+    console.log(id);
+    document.getElementById('advhead').innerHTML = 'Advice #' + id;
+    document.getElementById('adv').innerHTML ='"'+ quotes+'"';
+
+}
+advice();
+function changequote()
+{
+    advice();
+}
