af4205b5249a4aff3f5e1213d63c55a0c209a678
Ajay Prasath M <ajay3015@global.chainsys.com>
2022-03-23 18:26:00 +0530

    space js updation

--- space-tourism/space-tourism/starter-code/script/space.js
+++ space-tourism/space-tourism/starter-code/script/space.js
@@ -11,35 +11,35 @@ fetch(URL)
     });
     let spacetitle = document.getElementById('spacehead');
     let spaceText = document.getElementById('spacepragraph');
-    let Distance = document.getElementById('distance');
-    let Timeset = document.getElementById('time');
+    let distance = document.getElementById('distance');
+    let timeset = document.getElementById('time');
     let setplanet = document.getElementById('planetimg');
     let bio = document.getElementById('bio');
-    let Activeslide = document.getElementById('findlength').getElementsByClassName('circle');
+    let activeslide = document.getElementById('findlength').getElementsByClassName('circle');
     var slidelength = document.getElementById('findlength').getElementsByTagName('li').length;
-    let Menubar = document.getElementById('menu');
-    let Listmenu = document.getElementById('slideval');
+    let menubar = document.getElementById('menu');
+    let listmenu = document.getElementById('slideval');
     console.log(slidelength);
 
-    Menubar.addEventListener('click',function(){
+    menubar.addEventListener('click',function(){
         var img1 = "file:///D:/Htmlrepo/AngularNode/space-tourism/space-tourism/starter-code/shared/icon-hamburger.svg";
         var img2 = "file:///D:/Htmlrepo/AngularNode/space-tourism/space-tourism/starter-code/shared/icon-close.svg";
         var img3 = "https://html-lessons-six.vercel.app/space-tourism/space-tourism/starter-code/shared/icon-hamburger.svg";
         var img4 = "https://html-lessons-six.vercel.app/space-tourism/space-tourism/starter-code/shared/icon-close.svg";
-        console.log(Menubar.src);
-        if(Menubar.src === img1 || Menubar.src === img3)
+        console.log(menubar.src);
+        if(menubar.src === img1 || menubar.src === img3)
         {
-            Menubar.src = "./shared/icon-close.svg";
+            menubar.src = "./shared/icon-close.svg";
         }
-        else if(Menubar.src === img2 || Menubar.src === img4){
-            Menubar.src = "./shared/icon-hamburger.svg";
+        else if(menubar.src === img2 || menubar.src === img4){
+            menubar.src = "./shared/icon-hamburger.svg";
         }
-        Listmenu.classList.toggle('display');
+        listmenu.classList.toggle('display');
     })
 
     var initial = 0;
     var crewslide = 0;
-    var Autoslide = setInterval(function()
+    var autoslide = setInterval(function()
     {
         if(initial == slidelength)
         {
@@ -69,8 +69,8 @@ function slidechange(event)
       
     spacetitle.innerHTML = setSpace[currentpagename][event].name;
     spaceText.innerHTML = setSpace[currentpagename][event].description;
-    Distance.innerHTML = setSpace[currentpagename][event].distance;
-    Timeset.innerHTML = setSpace[currentpagename][event].travel;
+    distance.innerHTML = setSpace[currentpagename][event].distance;
+    timeset.innerHTML = setSpace[currentpagename][event].travel;
     setplanet.src = setSpace[currentpagename][event].images.webp;
     }
     else if(currentpagename == 'crew')
@@ -88,7 +88,7 @@ function slidechange(event)
          setplanet.src = setSpace[currentpagename][event].images.portrait;
 
         spacetitle.innerHTML = setSpace[currentpagename][event].name;
-        Activeslide[event].classList.toggle("active");
+        activeslide[event].classList.toggle("active");
        
     }
 }
