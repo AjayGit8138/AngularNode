444f140c02db08dc446a16af74352842d99649c8
Ajay Prasath M <ajay3015@global.chainsys.com>
2022-03-26 18:12:31 +0530

    Pathchanges for js assignment

---
+++ jsmodule/element.js
@@ -0,0 +1,39 @@
+import { setcurrencies } from "./currencyoption.js";
+import { calculate } from "./currencyconversion.js";
+
+
+$( document ).ready(function() {
+    setcurrencies();
+});
+function setmoney()
+{
+    console.log("Function call from element.jsfile");
+    setcurrencies();
+}
+window.setmoney = setmoney;
+
+
+var listen = document.getElementById('inputvalue');
+console.log(listen);
+listen.addEventListener("change",function(){
+    calculate();
+})
+// var dropdown = document.getElementById('money');
+// dropdown.addEventListener("mouseover",function(){
+//     // var droplength = document.getElementById('money').options.length;
+//          document.getElementById('money').size = '5';
+// })
+
+// var tochange = document.getElementById('toconversion');
+// tochange.addEventListener("mouseover",function(){
+//     var droplength = document.getElementById('toconversion').options.length;
+//         document.getElementById('toconversion').size = droplength;
+// })
+        
+
+
+
+
+
+
+
