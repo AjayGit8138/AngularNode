70cd27f4a19afea7871530c12723a3c5d4cc28f4
Ajay Prasath M <ajay3015@global.chainsys.com>
2022-03-26 18:45:19 +0530

    jsmoduleupdated

--- jsmodule/element.js
+++ jsmodule/element.js
@@ -16,20 +16,6 @@ console.log(listen);
 listen.addEventListener("change",function(){
     calculate();
 })
-// var dropdown = document.getElementById('money');
-// dropdown.addEventListener("mouseover",function(){
-//     // var droplength = document.getElementById('money').options.length;
-//          document.getElementById('money').size = '5';
-// })
-
-// var tochange = document.getElementById('toconversion');
-// tochange.addEventListener("mouseover",function(){
-//     var droplength = document.getElementById('toconversion').options.length;
-//         document.getElementById('toconversion').size = droplength;
-// })
-        
-
-
 
 
 
