d19ab358ddde22b45a46506fb7d881a878e15679
Ajay Prasath M <ajay3015@global.chainsys.com>
2022-03-28 09:58:46 +0530

    Htmlchallenges,css vercel path changes

--- jsmodule/currencyconversion.js
+++ jsmodule/currencyconversion.js
@@ -3,6 +3,6 @@ export function calculate() {
     let x = document.forms["myForm"]["currency"].value;
     let y = document.forms["myForm"]["toconversion"].value;
     let input = document.forms["myForm"]["inputval"].value;
-    let output = Math.floor((exchageval * input)*10000);
+    let output = Math.floor((exchageval * input)*1000);
     document.getElementById('converted').value = `$${output}`;
   }
